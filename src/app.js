c2841cdcb28a26fafedd418633cde4b99c72d0dd
vanman9599 <vanjordan46@hotmail.com>
2022-07-21 10:57:29 -0700

    dropdown

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React,  {useState}  from 'react';
 import Accordion from './components/Accordion'
 import Search from './components/Search'
+import Dropdown from './components/Dropdown'
+
+const options = [
+    {
+        label: 'Red',
+        value: 'red'
+    }, 
+    {
+        label: 'Green', 
+        value: 'green',
+    }, 
+    {
+        label: 'Blue', 
+        value: 'blue'
+    }
+]
 
 const items = [
     { 
@@ -17,6 +33,9 @@ const items = [
     }
 ]
 
-export default () => {
-    return <div><Search /></div>
-}
+const App = () => {
+    const [selected, setSelected] = useState(options[0])
+    return <div><Dropdown onSelectedChange={setSelected} selected={selected} options={options}/></div>
+} 
+
+export default App;
---
+++ src/components/Dropdown.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+const Dropdown = ({options, selected, onSelectedChange })=>{
+    const [open,setOpen] = useState(false);
+
+    const renderedOptions = options.map((option)=>{
+        if(option.value===selected.value){
+            return null;
+        }
+        return(
+            <div onClick={()=> onSelectedChange(option)} key={option.value} className="item">
+                {option.label}
+            </div>
+        )
+    })
+    return (
+        <div className="ui form">
+            <div className='field'>
+                <label className="label">
+                    Select a color
+                </label>
+                <div onClick={()=> setOpen(!open)} className={`ui selection dropdown ${open ?'visible active':''}`}>
+                    <i className="dropdown icon"></i>
+                    <div className="text">{selected.label}</div>
+                    <div className={`menu ${open ? 'visible transition': '' }`}>
+                        {renderedOptions}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Dropdown;
