367da2ad0343206179db1224bc356a6a847b679b
vanman9599 <vanjordan46@hotmail.com>
2022-07-23 18:50:27 -0700

    translate/convert done

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React,  {useState}  from 'react';
 import Accordion from './components/Accordion'
 import Search from './components/Search'
 import Dropdown from './components/Dropdown'
+import Translate from './components/Translate';
 
 const options = [
     {
@@ -34,8 +35,14 @@ const items = [
 ]
 
 const App = () => {
-    const [selected, setSelected] = useState(options[0])
-    return <div><Dropdown onSelectedChange={setSelected} selected={selected} options={options}/></div>
+    
+   
+    return (
+        <div>
+            <Translate />
+        </div>
+    )
+       
 } 
 
 export default App;
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-const Dropdown = ({options, selected, onSelectedChange })=>{
+const Dropdown = ({options, selected, onSelectedChange, label })=>{
     const [open,setOpen] = useState(false);
-
+    const ref = useRef();
+    useEffect(() => {
+    const onBodyClick = (event) => {
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+      setOpen(false);
+    };
+    document.body.addEventListener("click", onBodyClick, { capture: true });
+ 
+    return () => {
+      document.body.removeEventListener("click", onBodyClick, {
+        capture: true,
+      });
+    };
+  }, []);
     const renderedOptions = options.map((option)=>{
         if(option.value===selected.value){
             return null;
@@ -14,10 +29,10 @@ const Dropdown = ({options, selected, onSelectedChange })=>{
         )
     })
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className='field'>
                 <label className="label">
-                    Select a color
+                    {label}
                 </label>
                 <div onClick={()=> setOpen(!open)} className={`ui selection dropdown ${open ?'visible active':''}`}>
                     <i className="dropdown icon"></i>
