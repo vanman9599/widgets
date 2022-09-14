fa0cdfb99c789ba8e60e0b133e004b2282b09e31
van <vanjordan46@hotmail.com>
2022-09-14 14:53:10 -0700

    link, routes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import Accordion from './components/Accordion'
 import Search from './components/Search'
 import Dropdown from './components/Dropdown'
 import Translate from './components/Translate';
+import Route from './components/Route'
+import Header from './components/Header'
 
 const options = [
     {
@@ -34,12 +36,53 @@ const items = [
     }
 ]
 
+const showAccordion = () =>{
+    if(window.location.pathname === '/'){
+        return <Accordion items={items} />   
+    }
+}
+
+const showList = () =>{
+    if(window.location.pathname === '/list'){
+        return <Search />   
+    }
+}
+
+const showDropdown = () =>{
+    if(window.location.pathname === '/dropdown'){
+        return <Dropdown />   
+    }
+}
+
+const showTranslate = () =>{
+    if(window.location.pathname === '/translate'){
+        return <Translate />   
+    }
+}
+
+
 const App = () => {
-    
+    const [selected, setSelected] = useState(options[0])
    
     return (
         <div>
-            <Translate />
+            <Header />
+           <Route path="/">
+            <Accordion items={items}/>
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown label="Select a color"
+                options={options}
+                selected={selected}
+                onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>
     )
        
